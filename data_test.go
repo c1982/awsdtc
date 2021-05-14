c75ba782c844416576c7cf8d3c611347ad109238
c1982 <aspsrc@gmail.com>
2021-05-14 17:08:30 +0300

    feat: added new request handler json response
    * enabled date selected
    * refactor javascripts

--- data_test.go
+++ data_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestParseUsageType(t *testing.T) {
 	testcases := []struct {
@@ -54,6 +57,44 @@ func TestParseRegionalType(t *testing.T) {
 		}
 		if direction != v.Dir {
 			t.Errorf("invalid dst have: %s, got: %s", v.Dir, direction)
+		}
+	}
+}
+
+func TestGetDates(t *testing.T) {
+	testcases := []struct {
+		In    string
+		Start string
+		End   string
+		Err   error
+	}{
+		{"2020|January", "2020-01-01", "2020-01-31", nil},
+		{"2020|April", "2020-04-01", "2020-04-30", nil},
+		{"2020|December", "2020-12-01", "2020-12-31", nil},
+		{"2021|December", "2021-12-01", "2021-12-31", nil},
+		{"2020|a", "", "", errors.New("")},
+		{"2020", "", "", errors.New("")},
+		{"2020|0", "", "", errors.New("")},
+	}
+
+	for _, v := range testcases {
+		start, end, err := GetDates(v.In)
+		if v.Err == nil {
+			if err != nil {
+				t.Error(err)
+			}
+		} else {
+			if err == nil {
+				t.Error(err)
+			}
+		}
+
+		if v.Start != start {
+			t.Errorf("invalid start value. have: %s, got: %s", v.Start, start)
+		}
+
+		if v.End != end {
+			t.Errorf("invalid end value. have: %s, got: %s", v.End, end)
 		}
 	}
 }
--- web.go
+++ web.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func RunPage() {
 	index := template.Must(template.ParseFiles("./pages/index.html"))
-
 	http.HandleFunc("/datatransfers", func(w http.ResponseWriter, r *http.Request) {
+		billdate := r.URL.Query().Get("date")
+		if billdate == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "%s", errors.New("date parameter cannot be empty"))
+			return
+		}
 
-	})
-
-	http.HandleFunc("/regionaldatatransfers", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		start := "2021-05-01"
-		end := "2021-05-13"
-
-		report, regionalreport, err := GenerateDataMock(start, end, "MONTHLY")
+		start, end, err := GetDates(billdate)
 		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+
+		report, regionalreport, err := GenerateData(start, end, "MONTHLY")
+		if err != nil {
+			log.Printf("error: %s, start: %s, end: %s", err, start, end)
+			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprintf(w, "%s", err)
 			return
 		}
@@ -35,8 +43,24 @@ func RunPage() {
 			Regions: regionalreport,
 		}
 
-		index.Execute(w, data)
+		rspdata, err := json.Marshal(data)
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "%s", rspdata)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		index.Execute(w, nil)
 	})
 
 	http.ListenAndServe(":8000", nil)
+}
+
+func GetDates(billdate string) (startdate, enddate string, err error) {
+	start, err := time.Parse("2006|January", billdate)
+	if err != nil {
+		return startdate, enddate, err
+	}
+	end := start.AddDate(0, 1, -1)
+	return start.Format("2006-01-02"), end.Format("2006-01-02"), nil
 }
